389a33adb8f23e63d49462e4182a6f458de40136
Haokai Zhao <email@example.com>
2020-10-03 14:37:32 +0800

    update schema.sql

--- cs3311/exercise/w3/schema.sql
+++ cs3311/exercise/w3/schema.sql
@@ -5,22 +5,29 @@ create table Employees (
 	tfn         char(11) check (tfn ~ '[0-9]{3}-[0-9]{3}-[0-9]{3}'),
 	givenName   varchar(30) not null,
 	familyName  varchar(30),
-	hoursPweek  float check (hoursPweek between 0 and 168)
+	hoursPweek  float check (hoursPweek between 0 and 168),
+	primary key (tfn)
 );
 
 create table Departments (
 	id          char(3) check (id ~'[0-9]{3}'),
-	name        varchar(100) unique,
-	manager     char(11) unique
+	name        varchar(100) not null unique,
+	manager     char(11) not null unique,
+	primary key (id),
+	foreign key (manager) references Employees(tfn)
 );
 
 create table DeptMissions (
 	department  char(3),
-	keyword     varchar(20)
+	keyword     varchar(20),
+	primary key (department, keyword),
+	foreign key (department) references Departments(id)
 );
 
 create table WorksFor (
-	employee    char(11),
+	employee    char(11) not null,
 	department  char(3),
-	percentage  float check (percentage between 0 and 100)
+	percentage  float check (percentage between 0 and 100),
+	foreign key (employee) references Employees(tfn),
+	foreign key (department) references Departments(id)
 );
