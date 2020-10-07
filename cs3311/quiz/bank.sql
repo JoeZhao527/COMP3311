f0c4529e9be9ac69e19ca9023fa37342f1106118
Haokai Zhao <email@example.com>
2020-10-07 22:09:15 +0800

    fix syntax

--- cs3311/quiz/bank.sql
+++ cs3311/quiz/bank.sql
@@ -1,13 +1,13 @@
-create table Account {
+create table Account (
     accountNo   char(5),
     branchName  text not null,
     balance     integer not null
     primary key (accountNo)
-};
+);
 
-create table Owner {
+create table Owner (
     account     char(5),
     customer    integer,
     primary key (account),
     foreign key (account) references Account(accountNo)
-};
+);
